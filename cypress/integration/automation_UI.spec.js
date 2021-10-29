35e3648db98a96b63d3d07e31d6f60f4c98e7944
TarasovArtem <Hans8883@yandex.ru>
2021-10-29 20:05:23 +0200

    added verifications of Countres and Bday

--- cypress/integration/automation_UI.spec.js
+++ cypress/integration/automation_UI.spec.js
@@ -14,6 +14,15 @@ describe('Automation Task - UI', () => {
     it('Should shows the automation test of UI', () => {
         automation.cookieBanner();
         automation.startNowBtn();
-        automation.whereYouLiveTxt('Which country do you live in?')
+        automation.titlePage('Which country do you live in?');
+        automation.livingCountry_fieldset();
+        automation.walesRadioBtn();
+        automation.nextBtn();
+        automation.titlePage('What is your date of birth?');
+        automation.nextBtn(); //Skipping filling in the date of birth fields
+        automation.errorMessage()// getting Error message
+        automation.dateOfbirth('31', '07', '1961');
+        automation.nextBtn();
+
     })
 })
--- cypress/page-object/automation_UI.js
+++ cypress/page-object/automation_UI.js
@@ -10,8 +10,40 @@ export class Automation {
         cy.contains('Start now').click();
     }
 
-    whereYouLiveTxt(Text) {
+    titlePage(Text) {
         cy.title().should('include', Text)
     }
+
+    livingCountry_fieldset() {
+        cy.get('#livingCountry_fieldset > label').should(($livingCountry) => {
+            expect($livingCountry).to.contain('England')
+            expect($livingCountry).to.contain('Scotland')
+            expect($livingCountry).to.contain('Wales')
+            expect($livingCountry).to.contain('Northern Ireland')
+        })
+    }
+
+    walesRadioBtn() {
+        cy.get('#label-wales').click().should('have.class', 'selected');
+    }
+
+    nextBtn() {
+        cy.get('#next-button').click();
+    }
+
+    errorMessage() {
+        cy.get('h2#error-summary-heading').should('have.text', '\n\t\t\t\t\t\tThere is a problem\n        \t')
+        cy.get('.error-message a').should('have.attr', 'href');
+        cy.contains('Enter your date of birth').click();
+    }
+
+    dateOfbirth(Day, Month, Year) {
+        cy.get('#dob-day').type(Day);
+        cy.get('#dob-month').type(Month);
+        cy.get('#dob-year').type(Year);
+    }
+
+
+
 }
 export const automation = new Automation();
