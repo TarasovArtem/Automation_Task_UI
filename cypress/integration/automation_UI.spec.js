3e03b120d39832ca54709c4a8c74653c7f76d682
TarasovArtem <Hans8883@yandex.ru>
2021-10-29 10:40:42 +0200

    first commit

---
+++ cypress/integration/automation_UI.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+import Navigation from '../page-object/navigation';
+
+describe('', () => {
+    const navigation = new Navigation();
+
+    beforeEach(() => {
+        navigation.navigate();
+    })
+
+    it('', () => {
+
+    })
+})
