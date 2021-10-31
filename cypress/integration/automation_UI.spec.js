a071982e55b9d0446e38b3747cc81ba3638ddd11
TarasovArtem <Hans8883@yandex.ru>
2021-10-31 10:30:51 +0100

    Added comments

--- cypress/integration/automation_UI.spec.js
+++ cypress/integration/automation_UI.spec.js
@@ -7,12 +7,12 @@ import {automation} from '../page-object/automation_UI'
 
 describe('Automation Task - UI', () => {
     before(() => {
-        navigation.navigate();
+        navigation.navigate(); // Opens the website
+        automation.cookieBanner(); // Closes cookie banner
     })
 
     beforeEach(() => {
-        //automation.cookieBanner();
-        Cypress.Cookies.preserveOnce('SESSION', 'nhsuk-cookie-consent')       
+        Cypress.Cookies.preserveOnce('SESSION', 'nhsuk-cookie-consent')// Preserves Cookies       
     })
 
     it('Should show the page with name: Check what help you could get to pay for NHS costs', () => {    
@@ -22,7 +22,7 @@ describe('Automation Task - UI', () => {
 
     it('Should shows the page with name: Which country do you live in?', () => {
         automation.titlePage('Which country do you live in?');
-        automation.livingCountry_fieldset();
+        automation.livingCountry_fieldset(); 
         automation.walesRadioBtn();
         automation.nextBtn();
     })
@@ -38,7 +38,7 @@ describe('Automation Task - UI', () => {
 
     it('Should shows the page with name: Do you live with a partner?', () => {
         automation.titlePage('Do you live with a partner?');
-        automation.nextBtn();
+        automation.nextBtn(); //Skipping filling in the date of birth fields
         automation.errorMessage('\n\t\t\t\t\t\tThere is a problem\n        \t');
         automation.yesCheckbox();
         automation.nextBtn();
@@ -46,7 +46,7 @@ describe('Automation Task - UI', () => {
 
     it('Should shows the page with name: Do you or your partner claim any benefits or tax credits?', () => {
         automation.titlePage('Do you or your partner claim any benefits or tax credits?');
-        automation.nextBtn();
+        automation.nextBtn(); //Skipping filling in the date of birth fields
         automation.errorMessage('\n\t\t\t\t\t\tThere is a problem\n        \t');
         automation.noCheckbox();
         automation.nextBtn();
@@ -54,7 +54,7 @@ describe('Automation Task - UI', () => {
 
     it('Should shows the page with name: Are you pregnant or have you given birth in the last 12 months?', () => {
         automation.titlePage('Are you pregnant or have you given birth in the last 12 months?');
-        automation.nextBtn();
+        automation.nextBtn();//Skipping filling in the date of birth fields
         automation.errorMessage('\n\t\t\t\t\t\tThere is a problem\n        \t');
         automation.noCheckbox();
         automation.nextBtn();
@@ -62,7 +62,7 @@ describe('Automation Task - UI', () => {
 
     it('Should shows the page with name: Do you have an injury or illness caused by serving in the armed forces?', () => {
         automation.titlePage('Do you have an injury or illness caused by serving in the armed forces?');
-        automation.nextBtn();
+        automation.nextBtn();//Skipping filling in the date of birth fields
         automation.errorMessage('\n\t\t\t\t\t\tThere is a problem\n        \t');
         automation.noCheckbox();
         automation.nextBtn();
@@ -70,7 +70,7 @@ describe('Automation Task - UI', () => {
 
     it('Should shows the page with name: Do you or your partner live permanently in a care home?', () => {
         automation.titlePage('Do you or your partner live permanently in a care home?');
-        automation.nextBtn();
+        automation.nextBtn();//Skipping filling in the date of birth fields
         automation.errorMessage('\n\t\t\t\t\t\tThere is a problem\n        \t');
         automation.noCheckbox();
         automation.nextBtn();
@@ -78,7 +78,7 @@ describe('Automation Task - UI', () => {
 
     it('Should shows the page with name: Do you and your partner have more than £16,000 in savings, investments or property?', () => {
         automation.titlePage('Do you and your partner have more than £16,000 in savings, investments or property?');
-        automation.nextBtn();
+        automation.nextBtn();//Skipping filling in the date of birth fields
         automation.errorMessage('\n\t\t\t\t\t\tThere is a problem\n        \t');
         automation.noCheckbox();
         automation.nextBtn();
